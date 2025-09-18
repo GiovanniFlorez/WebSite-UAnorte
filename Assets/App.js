66cfa80abc5bf4026f05638aeadc371e038543a3
GiovanniFlorez <giovanniflorez22@gmail.com>
2025-09-18 17:17:55 -0500

    Update App.js

--- Assets/App.js
+++ Assets/App.js
@@ -1,10 +1,51 @@
+/*-------------Movimiento de imágenes del slider con flechas y automático--------*/
 const images = document.querySelectorAll(".slider img");
-let index = 0;
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 
-function showNextImage() {
-  images[index].classList.remove("active");
-  index = (index + 1) % images.length;
-  images[index].classList.add("active");
+let index = 0;
+let interval; 
+
+function showImage(i) {
+  images.forEach(img => img.classList.remove("active"));
+  images[i].classList.add("active");
 }
 
-setInterval(showNextImage, 3000);
+function nextImage() {
+  index = (index + 1) % images.length;
+  showImage(index);
+}
+
+function prevImage() {
+  index = (index - 1 + images.length) % images.length;
+  showImage(index);
+}
+
+nextBtn.addEventListener("click", () => {
+  nextImage();
+  resetInterval();
+});
+
+prevBtn.addEventListener("click", () => {
+  prevImage();
+  resetInterval();
+});
+
+function startInterval() {
+  interval = setInterval(nextImage, 3000);
+}
+
+function resetInterval() {
+  clearInterval(interval);
+  startInterval();
+}
+
+showImage(index);
+startInterval();
+
+/*----------------------------Agregar el footer----------*/
+fetch("../SRC/footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
