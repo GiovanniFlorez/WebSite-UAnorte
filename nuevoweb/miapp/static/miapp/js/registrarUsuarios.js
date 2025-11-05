9f8cfd548b5d3f07924a6d9e3f68d4664df68312
GiovanniFlorez <giovanniflorez22@gmail.com>
2025-11-05 18:30:46 -0500

    Add files via upload

---
+++ nuevoweb/miapp/static/miapp/js/registrarUsuarios.js
@@ -0,0 +1,52 @@
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const usuario = document.getElementById('usuario');
+const password = document.getElementById('password');
+const form = document.getElementById("registroFormulario");
+
+nombre.addEventListener('invalid', function() {
+    this.setCustomValidity('Por favor, ingresa el nombre del usuario');
+});
+
+nombre.addEventListener('input', function() {
+    this.setCustomValidity('');
+});
+
+email.addEventListener('invalid', function() {
+    this.setCustomValidity('Por favor, ingresa el email del usuario');
+});
+
+email.addEventListener('input', function() {
+    this.setCustomValidity('');
+});
+
+usuario.addEventListener('invalid', function() {
+    this.setCustomValidity('Por favor, ingresa el usuario');
+});
+
+usuario.addEventListener('input', function() {
+    this.setCustomValidity('');
+});
+
+password.addEventListener('invalid', function() {
+    this.setCustomValidity('Por favor, ingresa una contraseña');
+});
+
+password.addEventListener('input', function() {
+    this.setCustomValidity('');
+});
+
+
+form.addEventListener("submit", function(e) {
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&._-])[A-Za-z\d@$!%*?&._-]{8,}$/;
+  
+  if (!regex.test(password.value)) {
+    e.preventDefault(); 
+    Swal.fire({
+      icon: 'error',
+      title: 'Contraseña inválida',
+      text: 'Debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial',
+      confirmButtonText: 'Entendido'
+    });
+  }
+});
