42b7a88ee9c0fa29805a8a1d6c7fd6c6accfec83
GiovanniFlorez <giovanniflorez22@gmail.com>
2025-11-07 16:32:15 -0500

    Add files via upload

--- nuevoweb/miapp/models.py
+++ nuevoweb/miapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Usuario(AbstractUser):
+    ES_EDITOR = 'editor'
+    ES_ADMIN = 'admin'
+
+    ROLES = [
+        (ES_EDITOR, 'Editor'),
+        (ES_ADMIN, 'Administrador'),
+    ]
+
+    rol = models.CharField(max_length=10, choices=ROLES, default=ES_EDITOR)
+
+    def es_admin(self):
+        return self.rol == self.ES_ADMIN
+    
+    def es_editor(self):
+        return self.rol == self.ES_EDITOR
+    
+
+class Contenido(models.Model):
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    imagen = models.ImageField(upload_to='imagenes/', blank=True, null=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titulo
--- nuevoweb/miapp/urls.py
+++ nuevoweb/miapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -6,5 +7,13 @@ urlpatterns = [
     path('enviar-contacto/', views.enviar_contacto, name='enviar_contacto'),
     path('enviar-pqrsf/', views.enviar_pqrsf, name='enviar_pqrsf'),
     path('logout/', views.cerrar_sesion, name='cerrar_sesion'),
+    
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+
+    path('login-redirigir/', views.login_redirigir, name='login_redirigir'),
+
+    path('crud-usuarios/', views.crud_usuarios, name='crud_usuarios'),  # Vista admin
+    path('editar-contenido/', views.editar_contenido, name='editar_contenido'),  # Vista editor
+
     path('<str:pagina>/', views.pagina_estatica, name='pagina_estatica'),
 ]
