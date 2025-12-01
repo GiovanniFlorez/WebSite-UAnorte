ed5cc0674385bc0e393f744d196c70a26b5a2ccb
GiovanniFlorez <giovanniflorez22@gmail.com>
2025-12-01 11:56:32 -0500

    Add files via upload

--- nuevoweb/miapp/admin.py
+++ nuevoweb/miapp/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Usuario, Noticia, SliderImage
 
+# REGISTRO DEL MODELO Usuario EN EL ADMINISTRADOR DE DJANGO
 class UsuarioAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'rol', 'is_staff', 'is_active', 'date_joined')
     list_filter = ('rol', 'is_staff', 'is_active', 'date_joined')
@@ -14,7 +15,7 @@ admin.site.register(Usuario, UsuarioAdmin)
 
 admin.site.register(Noticia)
 
-
+# REGISTRO DEL MODELO SliderImage EN EL ADMINISTRADOR DE DJANGO
 @admin.register(SliderImage)
 class SliderImageAdmin(admin.ModelAdmin):
     list_display = ("id", "image", "order")
